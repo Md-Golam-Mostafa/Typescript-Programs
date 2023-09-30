9a92108c720878edafc9c2bbc0bc26b6a1ceeda1
Md-Golam-Mostafa <gmshakilcse@gmail.com>
2023-09-30 07:46:19 +0600

    tuple type

---
+++ LevelOne/TupleType.js
@@ -0,0 +1,5 @@
+// mixed data - key, value
+var students;
+students = [100, "rohan"];
+students.push(100, "rapin");
+console.log(students);
---
+++ LevelOne/TupleType.ts
@@ -0,0 +1,6 @@
+// mixed data - key, value
+let students: [number, string];
+students = [100,"rohan"];
+
+students.push(100,"rapin");
+console.log(students);
